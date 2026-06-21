4bfb4852139dff254d8c2c1952946c6af5f8d395
Nick Arbuzov <nickarbuzov@yandex.by>
2026-06-21 23:31:34 +0400

    реф: сетап тестов

--- test/jest-e2e.setup.ts
+++ test/jest-e2e.setup.ts
@@ -8,5 +8,10 @@ process.env.JWT_ACCESS_SECRET ??= 'test-access-secret';
 process.env.JWT_REFRESH_SECRET ??= 'test-refresh-secret';
 process.env.JWT_ACCESS_TTL ??= '5m';
 process.env.JWT_REFRESH_TTL ??= '7d';
+process.env.MINIO_ENDPOINT ??= 'http://localhost:9000';
+process.env.MINIO_ACCESS_KEY ??= 'minio';
+process.env.MINIO_SECRET_KEY ??= 'minio-secret';
+process.env.MINIO_BUCKET ??= 'avatars-test';
+process.env.MINIO_REGION ??= 'us-east-1';
 
 jest.setTimeout(30000);
